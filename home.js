7fb74ac44b25077682cc22901055b1b8963ef522
Adams456 <161683457+Adams456@users.noreply.github.com>
2024-03-21 09:39:33 +0300

    Renamed some files
    made some changes in names to resolve the conflict

--- home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.home-slider', {
-        loop: true,
-        autoplay: {
-            delay: 5000, // Change slide every 5 seconds
-        },
-    });
-});
